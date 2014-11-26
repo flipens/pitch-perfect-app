54406170a25e22c941b775112fafce80b20b8062
Felipe Lloret <flipens@Felipes-MacBook-Pro.local>
2014-11-26 12:57:55 +0100

    first commit

---
+++ pitch/Classes/Models/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  pitch
+//
+//  Created by Felipe Lloret on 26/11/14.
+//  Copyright (c) 2014 Felipe Lloret. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    
+    var filePathUrl: NSURL!
+    var title: String!
+}
